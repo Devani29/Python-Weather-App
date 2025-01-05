13573a4cce238755750d7cd25c4789275c8fdf6d
Devani Moreno <devani.moreno@outlook.com>
2025-01-05 14:36:02 -0600

    Adding search box and logo

--- weather_app.py
+++ weather_app.py
@@ -12,4 +12,22 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+# Search box
+search_image=PhotoImage(file="search.png")
+myimage=Label(image=search_image)
+myimage.place(x=20,y=20)
+
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+search_icon=PhotoImage(file="search_icon.png")
+myimage_icon=Button(image=search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon.place(x=400,y=33)
+
+# logo
+logo_image=PhotoImage(file="logo.png")
+logo=Label(image=logo_image)
+logo.place(x=150,y=100)
+
 root.mainloop()
