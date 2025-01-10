9bb4f31ac94a17f374e4584b4d00d7ebccb4b7fb
Devani Moreno <devani.moreno@outlook.com>
2025-01-09 22:44:44 -0600

    Added labels and def getWeather

--- weather_app.py
+++ weather_app.py
@@ -12,6 +12,17 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+def getWeather():
+    city=textfield.get()
+
+    geolocator= Nominatim(user_agent="weather_app")
+    location= geolocator.geocode(city)
+    obj = TimezoneFinder()
+    result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
+    print(result)
+
+
+
 # Search box
 search_image=PhotoImage(file="search.png")
 myimage=Label(image=search_image)
@@ -22,12 +33,48 @@ textfield.place(x=50,y=40)
 textfield.focus()
 
 search_icon=PhotoImage(file="search_icon.png")
-myimage_icon=Button(image=search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon=Button(image=search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
 myimage_icon.place(x=400,y=33)
 
 # logo
 logo_image=PhotoImage(file="logo.png")
 logo=Label(image=logo_image)
 logo.place(x=150,y=100)
+
+# Botton box
+frame_image=PhotoImage(file="box.png")
+frame_myimage=Label(image=frame_image)
+frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+# label
+label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=120,y=400)
+
+label2=Label(root,text="HUMIDITY",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label2.place(x=250,y=400)
+
+label3=Label(root,text="DESCRIPTION",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label3.place(x=430,y=400)
+
+label4=Label(root,text="PRESSURE",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label4.place(x=650,y=400)
+
+t=Label(font=("arial",70,"bold"),fg="#ee666d")
+t.place(x=400,y=150)
+c=Label(font=("arial",15,"bold"))
+c.place(x=400,y=250)
+
+w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+w.place(x=120,y=430)
+h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+h.place(x=280,y=430)
+d=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+d.place(x=450,y=430)
+p=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+p.place(x=670,y=430)
+
+
+
+
 
 root.mainloop()
