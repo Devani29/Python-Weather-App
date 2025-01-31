cdd0a9fd2e5a2eca8913c63bf7cfc71bff77c043
Devani Moreno <devani.moreno@outlook.com>
2025-01-31 14:42:16 -0600

    Border issue fixed in test

--- prueba.py
+++ prueba.py
@@ -1,19 +1,31 @@
-# Crear el Canvas
-my_canvas = Canvas(root, width=900, height=500)
+# Crear la ventana principal
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
+from PIL import Image, ImageTk 
+from ctypes import windll
+
+root=tk.Tk()
+root.title("Banana's Weather App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
+root.iconbitmap('icono.ico')
+
+# Set image
+weather_bg = Image.open("bg_weather.jpg")
+weather_bg = weather_bg.resize((900, 500))
+weather_bg_tk = ImageTk.PhotoImage(weather_bg)
+
+# Create a canvas
+my_canvas = Canvas(root, width=900, height=500, highlightthickness=0)
 my_canvas.pack(fill="both", expand=True)
 
-# Crear un Label para el reloj
-clock = Label(root, font=("Helvetica", 20))
-clock.place(x=340, y=70)
+# # Set image in canvas
+my_canvas.create_image(0,0, image=weather_bg_tk, anchor="nw")
 
-# Agregar el Label al Canvas usando create_window
-clock_window = my_canvas.create_window(340, 70, window=clock)
-
-# Actualizar el texto del reloj
-def update_clock():
-    current_time = datetime.now().strftime("%I:%M %p")  # Hora actual
-    clock.config(text=current_time)  # Actualiza el texto del reloj
-    root.after(1000, update_clock)  # Llama a esta función cada 1000 ms (1 segundo)
-
-# Llamar a la función para actualizar el reloj
-update_clock()
+root.mainloop()
